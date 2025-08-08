78f0f52440abc50883cb16f460f27d09657129f2
vanessasousapro <vanessasousa.pro@gmail.com>
2025-08-08 14:21:30 -0300

    first commit

---
+++ src/test/java/com/vanessa/springlabs/spring_labs/SpringLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessa.springlabs.spring_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
